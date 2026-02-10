8481984b7308499b1a7e0ab0ee7b200a066e862f
l-lyun <kimdohyun032@gmail.com>
2026-02-10 19:31:25 +0900

    feat: 카카오 로그인 리다이렉트 get 적용

--- auth-server/src/main/java/com/truve/platform/user/service/controller/KakaoOAuthController.java
+++ auth-server/src/main/java/com/truve/platform/user/service/controller/KakaoOAuthController.java
@@ -6,9 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.truve.platform.common.response.ApiResult;
 import com.truve.platform.user.service.security.properties.KakaoOAuthProperties;
+import com.truve.platform.user.service.service.KakaoOAuthService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,9 +20,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class KakaoOAuthController {
 	private final KakaoOAuthProperties kakaoOAuthProperties;
-
+	private final KakaoOAuthService kakaoOAuthService;
 	@GetMapping("/login")
 	public ResponseEntity<Void> login() {
+		System.out.println("login");
 		String redirectUri =
 			kakaoOAuthProperties.getAuthorizationUrl()
 				+ "?response_type=code&client_id=" + kakaoOAuthProperties.getClientId()
@@ -31,5 +35,15 @@ public class KakaoOAuthController {
 			.build();
 	}
 
+	@GetMapping("/callback")
+	public ApiResult<Void> callback(
+		@RequestParam(required = false) String code,
+		@RequestParam(required = false) String error,
+		@RequestParam(required = false) String error_description,
+		@RequestParam(required = false) String state
+	) {
+		kakaoOAuthService.login(code, error, error_description, state);
+		return ApiResult.ok();
+	}
 
 }
--- auth-server/src/main/java/com/truve/platform/user/service/service/KakaoOAuthService.java
+++ auth-server/src/main/java/com/truve/platform/user/service/service/KakaoOAuthService.java
@@ -1,4 +1,17 @@
 package com.truve.platform.user.service.service;
 
+import org.springframework.stereotype.Service;
+
+import com.truve.platform.user.service.security.properties.KakaoOAuthProperties;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class KakaoOAuthService {
+	public final KakaoOAuthProperties kakaoOAuthProperties;
+
+	public void login(String code, String error, String errorDescription, String state) {
+
+	}
 }
