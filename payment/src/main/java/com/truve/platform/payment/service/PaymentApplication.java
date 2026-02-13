e77c841c85d882e4362a9c4215047b3d93b22190
YeKim1 <12202340@inha.edu>
2026-02-13 11:22:40 +0900

    feat(payment): 결제 입금 확인 웹훅 API 구현
    - 가상계좌 입금 확인을 위한 WebhookController 추가
    - 입금 요청 데이터를 WebhookEvent로 발행하는 로직 구현
    - 비동기 이벤트 처리를 위해 PaymentApplication에 @EnableAsync 추가

--- payment/src/main/java/com/truve/platform/payment/service/PaymentApplication.java
+++ payment/src/main/java/com/truve/platform/payment/service/PaymentApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication(scanBasePackages = "com.truve.platform")
 @EnableJpaAuditing
 @ConfigurationPropertiesScan
+@EnableAsync
 public class PaymentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
