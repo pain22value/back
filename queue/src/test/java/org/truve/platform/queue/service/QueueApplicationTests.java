75f1b2ca5e73bb517db8801fdbccaa37b2b9a2e7
l-lyun <kimdohyun032@gmail.com>
2026-02-15 17:05:08 +0900

    feat: queue-server 기반 셋팅

---
+++ queue/src/test/java/org/truve/platform/queue/service/QueueApplicationTests.java
@@ -0,0 +1,13 @@
+package org.truve.platform.queue.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
