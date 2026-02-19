dff0abb93a6e91e2ffb0cac3d3895ce6f15942ea
l-lyun <kimdohyun032@gmail.com>
2026-02-19 13:17:07 +0900

    feat: 대기열 유저 승급 스케줄러 구현

---
+++ queue/src/main/java/org/truve/platform/queue/service/queue/config/QueueProperties.java
@@ -0,0 +1,19 @@
+package org.truve.platform.queue.service.queue.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "queue")
+public class QueueProperties {
+
+	// TODO: 환경변수 동적 변경 필요
+	private int permitPerTick = 100;
+	private long schedulerDelayMs = 500;
+	private long readyTtlSec = 100;
+	private boolean schedulerEnabled = true;
+
+}
