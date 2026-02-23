64ff6aa584c1cf8f459bcfe179de6d0d3fea0158
l-lyun <kimdohyun032@gmail.com>
2026-02-23 14:52:23 +0900

    feat: ticketing server initial commit

---
+++ ticketing/src/main/java/org/truve/platform/ticketing/TicketingApplication.java
@@ -0,0 +1,16 @@
+package org.truve.platform.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+			TicketingApplication.class,
+			args
+		);
+	}
+
+}
