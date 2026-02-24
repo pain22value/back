d0ed03ea43275ab82da7f3d575894916462d7201
YeKim1 <12202340@inha.edu>
2026-02-24 11:08:34 +0900

    refactor: 애플리케이션 클래스에서 @EnableJpaAuditing 제거

--- payment/src/main/java/com/truve/platform/payment/service/PaymentApplication.java
+++ payment/src/main/java/com/truve/platform/payment/service/PaymentApplication.java
@@ -3,10 +3,8 @@ package com.truve.platform.payment.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = "com.truve.platform")
-@EnableJpaAuditing
 @ConfigurationPropertiesScan
 public class PaymentApplication {
 	public static void main(String[] args) {
