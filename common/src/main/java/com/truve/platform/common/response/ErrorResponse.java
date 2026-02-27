bbc365d153414fff8b1e802c6a763357d2ce0867
l-lyun <kimdohyun032@gmail.com>
2026-02-27 10:02:28 +0900

    refactor: error 응답 구조 에러코드 추가

--- common/src/main/java/com/truve/platform/common/response/ErrorResponse.java
+++ common/src/main/java/com/truve/platform/common/response/ErrorResponse.java
@@ -13,8 +13,8 @@ public class ErrorResponse {
 	private HttpStatus status;
 	private String message;
 
-	public static ResponseEntity<ErrorData> error(HttpStatus status, String message) {
-		return ResponseEntity.status(status).body(ErrorData.of(status.series().name(), message));
+	public static ResponseEntity<ErrorData> error(HttpStatus status, String message, String errorCode) {
+		return ResponseEntity.status(status).body(ErrorData.of(status.series().name(), message, errorCode));
 	}
 
 	@Getter
@@ -22,9 +22,9 @@ public class ErrorResponse {
 	public static class ErrorData {
 		private String code;
 		private String message;
-
-		public static ErrorData of(String code, String message) {
-			return new ErrorData(code, message);
+		private String errorCode;
+		public static ErrorData of(String code, String message, String errorCode) {
+			return new ErrorData(code, message, errorCode);
 		}
 	}
 }
