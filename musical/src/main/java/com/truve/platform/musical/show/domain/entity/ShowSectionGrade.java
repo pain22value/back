bdae5088aa10d1e45579f4c85f43d60c6e101fd4
l-lyun <kimdohyun032@gmail.com>
2026-03-04 14:13:25 +0900

    feat: 메인 서비스 좌석 정보 조회용 등급 테이블

--- musical/src/main/java/com/truve/platform/musical/show/domain/entity/ShowSectionGrade.java
+++ musical/src/main/java/com/truve/platform/musical/show/domain/entity/ShowSectionGrade.java
@@ -1,9 +1,7 @@
-package com.truve.platform.musical.pricing.domain.entity;
+package com.truve.platform.musical.show.domain.entity;
 
 import com.truve.platform.common.support.BaseEntity;
-import com.truve.platform.musical.show.domain.entity.Show;
 
-import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -18,28 +16,24 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "show_seat_grade")
-@AttributeOverride(name = "id", column = @Column(name = "show_seat_grade_id"))
-public class ShowSeatGrade extends BaseEntity {
+@Table(name = "show_section_grade")
+public class ShowSectionGrade extends BaseEntity {
+
 
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "show_id", nullable = false)
+	@JoinColumn(name = "show_id")
 	private Show show;
 
 	@Column(nullable = false, length = 50)
 	private String gradeName;
 
-	@Column(nullable = false)
-	private Integer basePrice;
-
 	@Column(length = 20)
 	private String colorCode;
 
 	@Builder
-	private ShowSeatGrade(Show show, String gradeName, Integer basePrice, String colorCode) {
+	public ShowSectionGrade(Show show, String gradeName, Long price, String colorCode) {
 		this.show = show;
 		this.gradeName = gradeName;
-		this.basePrice = basePrice;
 		this.colorCode = colorCode;
 	}
 }
