ec037a9698aec03396749b43b3a79b005ee5a420
YeKim1 <12202340@inha.edu>
2026-03-06 01:02:36 +0900

    feat: S3 관련 의존성 추가 및 이미지 URL 생성을 위한 S3Service 구현

---
+++ musical/src/main/java/com/truve/platform/musical/s3/S3Service.java
@@ -0,0 +1,19 @@
+package com.truve.platform.musical.s3;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class S3Service {
+	@Value("${s3.endpoint}")
+	private String endpoint;
+	@Value("${s3.bucket}")
+	private String bucketName;
+
+	public String getImageUrl(String fileName) {
+		return String.format("%s/%s/%s", endpoint, bucketName, fileName);
+	}
+}
