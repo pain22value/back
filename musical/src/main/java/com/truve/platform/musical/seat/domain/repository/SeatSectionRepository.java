97eec19a27bc01b768f8dc1ba616583dff031993
l-lyun <kimdohyun032@gmail.com>
2026-03-06 11:12:11 +0900

    refactor: 엔티티 구조 및 폴더구조 변경사항 반영

--- musical/src/main/java/com/truve/platform/musical/seat/domain/repository/SeatSectionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.truve.platform.musical.seat.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.truve.platform.musical.seat.domain.entity.SeatSection;
-
-public interface SeatSectionRepository extends JpaRepository<SeatSection, Long> {
-}
