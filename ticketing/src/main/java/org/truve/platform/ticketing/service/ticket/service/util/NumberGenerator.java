52d61d6f3be2872fc11013c86ac3f02ab37b49f5
YeKim1 <12202340@inha.edu>
2026-03-09 03:29:10 +0900

    feat: 예매 및 티켓 번호 생성을 위한 NumberGenerator 구현

---
+++ ticketing/src/main/java/org/truve/platform/ticketing/service/ticket/service/util/NumberGenerator.java
@@ -0,0 +1,20 @@
+package org.truve.platform.ticketing.service.ticket.service.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class NumberGenerator {
+	public String generateReservationNumber() {
+		String datePrefix = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+		String randomSuffix = UUID.randomUUID().toString().substring(0, 6).toUpperCase();
+		return "R" + datePrefix + randomSuffix;
+	}
+
+	public String generateTicketNumber() {
+		return "T-" + UUID.randomUUID().toString().substring(0, 13).toUpperCase();
+	}
+}
