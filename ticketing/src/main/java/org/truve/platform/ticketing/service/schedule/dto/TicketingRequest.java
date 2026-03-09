167cd1d19af44a6c86ad7e94b41875adafa01a73
l-lyun <kimdohyun032@gmail.com>
2026-03-09 10:40:20 +0900

    fix: 좌석 배열 NotNull -> NotEmpty

--- ticketing/src/main/java/org/truve/platform/ticketing/service/schedule/dto/TicketingRequest.java
+++ ticketing/src/main/java/org/truve/platform/ticketing/service/schedule/dto/TicketingRequest.java
@@ -2,17 +2,16 @@ package org.truve.platform.ticketing.service.schedule.dto;
 
 import java.util.List;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 public class TicketingRequest {
 
 	@Getter
 	@AllArgsConstructor
 	public static class HoldSeat {
-		@NotNull
+		@NotEmpty
 		List<Long> seatIds;
 	}
 }
