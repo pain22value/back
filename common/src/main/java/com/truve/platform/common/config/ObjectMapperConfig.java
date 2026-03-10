4bdabf06f6d7847be6c0f8061a45852eb8b95a43
l-lyun <kimdohyun032@gmail.com>
2026-03-10 23:28:06 +0900

    fix: ObjectMapperConfig 수정

---
+++ common/src/main/java/com/truve/platform/common/config/ObjectMapperConfig.java
@@ -0,0 +1,17 @@
+package com.truve.platform.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+
+@Configuration
+public class ObjectMapperConfig {
+	@Bean
+	public ObjectMapper objectMapper() {
+		return JsonMapper.builder()
+			.findAndAddModules()
+			.build();
+	}
+}
