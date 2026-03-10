87c1390b6a9366ee5251bca0682a5edca7872044
seungh22 <seunghee8898@naver.com>
2026-03-10 09:31:05 +0900

    feat(show): 배우 좋아요 예외 코드 M02/M03 추가

--- common/src/main/java/com/truve/platform/common/exception/ErrorCode.java
+++ common/src/main/java/com/truve/platform/common/exception/ErrorCode.java
@@ -50,7 +50,9 @@ public enum ErrorCode {
 	INVALID_HOLD_SEAT(HttpStatus.BAD_REQUEST, "타인이 점유한 좌석입니다.", "T11"),
 
 
-	NOT_FOUND_SHOW(HttpStatus.NOT_FOUND, "존재하지 않는 공연입니다.", "M01");
+	NOT_FOUND_SHOW(HttpStatus.NOT_FOUND, "존재하지 않는 공연입니다.", "M01"),
+    NOT_FOUND_ARTIST(HttpStatus.NOT_FOUND, "존재하지 않는 배우입니다.", "M02"),
+    ALREADY_LIKED_ARTIST(HttpStatus.BAD_REQUEST, "이미 좋아요한 배우입니다.", "M03"),;
 	;
 
 
