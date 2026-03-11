0bed3bb62c2335684d2266314ff797b5ffee9d62
YeKim1 <12202340@inha.edu>
2026-03-11 16:32:14 +0900

    feat: Kafka 이벤트 발행 시 event-type 헤더 추가
    - EventPublisher 인터페이스에 event-type 파라미터 추가
    - KafkaEventPublisher 구현체에 event-type 헤더 설정 로직 구현
    - ProducerRecord 헤더에 event-type을 바이트 배열로 추가
    - 이벤트 유형 정보를 Kafka 헤더를 통해 전달하도록 개선

--- common/src/main/java/com/truve/platform/common/event/EventPublisher.java
+++ common/src/main/java/com/truve/platform/common/event/EventPublisher.java
@@ -2,4 +2,6 @@ package com.truve.platform.common.event;
 
 public interface EventPublisher {
 	void publish(String topic, String key, Object event);
+
+	void publish(String topic, String key, String type, Object event);
 }
--- common/src/main/java/com/truve/platform/common/event/KafkaEventPublisher.java
+++ common/src/main/java/com/truve/platform/common/event/KafkaEventPublisher.java
@@ -1,12 +1,13 @@
 package com.truve.platform.common.event;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.truve.platform.common.exception.CustomException;
 import com.truve.platform.common.exception.ErrorCode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,6 +27,20 @@ public class KafkaEventPublisher implements EventPublisher {
 			throw new CustomException(ErrorCode.EVENT_SERIALIZATION_FAILED);
 		} catch (Exception exception) {
 			throw new CustomException(ErrorCode.EVENT_PUBLISH_FAILED);
+		}
+	}
+
+	@Override
+	public void publish(String topic, String key, String type, Object event) {
+		try {
+			String payload = objectMapper.writeValueAsString(event);
+
+			ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, payload);
+			record.headers().add("event-type", type.getBytes());
+
+			kafkaTemplate.send(record);
+		} catch (JsonProcessingException e) {
+			throw new CustomException(ErrorCode.EVENT_SERIALIZATION_FAILED);
 		}
 	}
 }
