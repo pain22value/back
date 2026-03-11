d4f9f67bb4f77b75671c123ec77250360d0c2888
l-lyun <kimdohyun032@gmail.com>
2026-03-11 15:13:52 +0900

    refactor: 티켓팅 입장권 userId UUID로 변경

--- ticketing/src/main/java/org/truve/platform/ticketing/service/ticketing/dto/AdmissionTokenClaimsDTO.java
+++ ticketing/src/main/java/org/truve/platform/ticketing/service/ticketing/dto/AdmissionTokenClaimsDTO.java
@@ -1,16 +1,18 @@
 package org.truve.platform.ticketing.service.ticketing.dto;
 
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class AdmissionTokenClaimsDTO {
-	private Long userId;
+	private UUID userId;
 	private Long showId;
 	private String tokenType;
 
-	public static AdmissionTokenClaimsDTO of(Long userId, Long showId, String tokenType) {
+	public static AdmissionTokenClaimsDTO of(UUID userId, Long showId, String tokenType) {
 		return new  AdmissionTokenClaimsDTO(userId, showId, tokenType);
 	}
 }
--- ticketing/src/main/java/org/truve/platform/ticketing/service/ticketing/dto/SessionTicketValueDTO.java
+++ ticketing/src/main/java/org/truve/platform/ticketing/service/ticketing/dto/SessionTicketValueDTO.java
@@ -1,15 +1,17 @@
 package org.truve.platform.ticketing.service.ticketing.dto;
 
+import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
 public class SessionTicketValueDTO {
-	private Long userId;
+	private UUID userId;
 	private Long showId;
 
-	public static SessionTicketValueDTO of(Long userId, Long showId) {
+	public static SessionTicketValueDTO of(UUID userId, Long showId) {
 		return new SessionTicketValueDTO(userId, showId);
 	}
 }
