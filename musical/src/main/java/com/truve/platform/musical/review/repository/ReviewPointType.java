edf4e0d77f549f2aee47b34d570e6b7188bbe2ec
l-lyun <kimdohyun032@gmail.com>
2026-03-11 12:04:56 +0900

    fix: 레포지토리 명 수정

--- musical/src/main/java/com/truve/platform/musical/review/repository/ReviewPointType.java
+++
@@ -1,6 +0,0 @@
-package com.truve.platform.musical.review.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewPointType extends JpaRepository<ReviewPointType, Long> {
-}
