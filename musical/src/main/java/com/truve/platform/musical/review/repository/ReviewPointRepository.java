a63e0f21c489b5c694abea204cc659aee002871b
l-lyun <kimdohyun032@gmail.com>
2026-03-12 20:18:33 +0900

    feat: 리뷰 포인트 엔티티 JPQL 쿼리 추가

--- musical/src/main/java/com/truve/platform/musical/review/repository/ReviewPointRepository.java
+++ musical/src/main/java/com/truve/platform/musical/review/repository/ReviewPointRepository.java
@@ -1,8 +1,29 @@
 package com.truve.platform.musical.review.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.truve.platform.musical.review.dao.ReviewPointCountDao;
 import com.truve.platform.musical.review.domain.entity.ReviewPoint;
 
+
 public interface ReviewPointRepository extends JpaRepository<ReviewPoint, Long> {
+
+	@Query(
+		"""
+	select ReviewPointDao(
+		rp.reviewPointType.point,
+		count(rp)
+		)
+	from ReviewPoint rp
+	join rp.review r
+	where r.showId = :showId
+		and r.deletedAt is null
+	group by rp.reviewPointType.point
+	"""
+	)
+	List<ReviewPointCountDao> countPointsByShowId(@Param("showId") Long showId);
 }
