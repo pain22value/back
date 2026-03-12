efa979a40cb6262da02060b5742784d416d00aa2
l-lyun <kimdohyun032@gmail.com>
2026-03-12 20:18:16 +0900

    feat: 리뷰 엔티티 조회 메서드 추가

--- musical/src/main/java/com/truve/platform/musical/review/repository/ReviewRepository.java
+++ musical/src/main/java/com/truve/platform/musical/review/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package com.truve.platform.musical.review.repository;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,13 @@ import com.truve.platform.musical.review.domain.entity.Review;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
 	boolean existsByUserIdAndShowId(UUID userId,  Long showId);
+
+	List<Review> findByShowIdAndDeletedAtIsNullOrderByCreatedAtDesc(Long showId);
+	List<Review> findByShowIdAndDeletedAtIsNullAndIsPositiveTrueOrderByCreatedAtDesc(Long showId);
+	List<Review> findByShowIdAndDeletedAtIsNullAndIsPositiveFalseOrderByCreatedAtDesc(Long showId);
+
+	long countByShowIdAndDeletedAtIsNull(Long showId);
+	long countByShowIdAndDeletedAtIsNullAndIsPositiveTrue(Long showId);
+
+
 }
