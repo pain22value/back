799a5769914fa06a0edda37dd1889dde919bca7d
l-lyun <kimdohyun032@gmail.com>
2026-03-12 13:34:14 +0900

    feat: Repository 내 필요 조회 메서드 추가

--- musical/src/main/java/com/truve/platform/musical/review/repository/ReviewPointTypeRepository.java
+++ musical/src/main/java/com/truve/platform/musical/review/repository/ReviewPointTypeRepository.java
@@ -1,8 +1,12 @@
 package com.truve.platform.musical.review.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.truve.platform.musical.review.domain.constant.ReviewPointName;
 import com.truve.platform.musical.review.domain.entity.ReviewPointType;
 
 public interface ReviewPointTypeRepository extends JpaRepository<ReviewPointType, Long> {
+	Optional<ReviewPointType> findByPoint(ReviewPointName name);
 }
--- musical/src/main/java/com/truve/platform/musical/review/repository/ReviewRepository.java
+++ musical/src/main/java/com/truve/platform/musical/review/repository/ReviewRepository.java
@@ -1,8 +1,12 @@
 package com.truve.platform.musical.review.repository;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.truve.platform.musical.review.domain.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+	boolean existsByUserIdAndShowId(UUID userId,  Long showId);
 }
