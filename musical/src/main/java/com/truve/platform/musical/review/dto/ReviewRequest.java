863b8b72a9e78278616be43514841b4484a461e6
l-lyun <kimdohyun032@gmail.com>
2026-03-12 13:34:58 +0900

    feat: ReviewService 리뷰 생성 로직 구현

---
+++ musical/src/main/java/com/truve/platform/musical/review/dto/ReviewRequest.java
@@ -0,0 +1,34 @@
+package com.truve.platform.musical.review.dto;
+
+import java.util.List;
+
+import com.truve.platform.musical.review.domain.constant.ReviewPointName;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class ReviewRequest {
+
+	@AllArgsConstructor
+	@Getter
+	public static class Create {
+
+		@NotNull
+		Boolean isPositive;
+
+		@NotNull
+		@Size(min = 1, max = 5)
+		List<ReviewPointName> emotionPoints;
+
+		@NotNull
+		@Size(min = 1, max = 5)
+		List<ReviewPointName> charmPoints;
+
+		@NotBlank
+		String content;
+
+	}
+}
---
+++ musical/src/main/java/com/truve/platform/musical/review/service/ReviewService.java
@@ -0,0 +1,80 @@
+package com.truve.platform.musical.review.service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.truve.platform.common.exception.CustomException;
+import com.truve.platform.common.exception.ErrorCode;
+import com.truve.platform.common.support.Preconditions;
+import com.truve.platform.musical.review.domain.entity.Review;
+import com.truve.platform.musical.review.domain.entity.ReviewPoint;
+import com.truve.platform.musical.review.domain.entity.ReviewPointType;
+import com.truve.platform.musical.review.dto.ReviewRequest;
+import com.truve.platform.musical.review.repository.ReviewPointRepository;
+import com.truve.platform.musical.review.repository.ReviewPointTypeRepository;
+import com.truve.platform.musical.review.repository.ReviewRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+
+	private final ReviewRepository reviewRepository;
+	private final ReviewPointTypeRepository reviewPointTypeRepository;
+	private final ReviewPointRepository reviewPointRepository;
+
+
+	@Transactional
+	public void create(UUID userId, Long showId, ReviewRequest.Create request) {
+
+		Preconditions.validate(
+			!reviewRepository.existsByUserIdAndShowId(userId, showId),
+			ErrorCode.ALREADY_EXIST_REVIEW
+		);
+
+		List<ReviewPointType> emotionPoints = request.getEmotionPoints().stream()
+			.map(emotionPoint -> {
+			Preconditions.validate(emotionPoint.isEmotionPoint(), ErrorCode.NOT_EMOTION_POINT);
+			return reviewPointTypeRepository.findByPoint(emotionPoint)
+				.orElseThrow(() -> new CustomException(ErrorCode.NOT_EMOTION_POINT));
+			}).toList();
+
+		List<ReviewPointType> charmPoints = request.getCharmPoints().stream()
+			.map(charmPoint -> {
+				Preconditions.validate(charmPoint.isCharmPoint(), ErrorCode.NOT_CHARM_POINT);
+				return reviewPointTypeRepository.findByPoint(charmPoint)
+					.orElseThrow(() -> new CustomException(ErrorCode.NOT_CHARM_POINT));
+			}).toList();
+
+
+		Review review = Review.builder().
+			showId(showId).
+			userId(userId).
+			content(request.getContent()).
+			isPositive(request.getIsPositive()).
+			// TODO: 티켓 및 예매 완료 시 연동
+				watchedAt(LocalDateTime.now()).
+			build();
+
+		Review savedReview =  reviewRepository.save(review);
+
+		List<ReviewPoint> reviewPoints = new ArrayList<>();
+
+
+		emotionPoints.forEach(emotionPoint -> {
+			reviewPoints.add(ReviewPoint.of(savedReview, emotionPoint));
+		});
+
+		charmPoints.forEach(charmPoint -> {
+			reviewPoints.add(ReviewPoint.of(savedReview, charmPoint));
+		});
+
+		reviewPointRepository.saveAll(reviewPoints);
+	}
+}
