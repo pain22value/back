6d51aac815fcad095d1b7cb521dc3b6651472434
YeKim1 <12202340@inha.edu>
2026-04-03 09:38:39 +0900

    test: PaymentClient MockitoBean을 테스트 컨텍스트에 추가

--- ticketing/src/test/java/org/truve/platform/ticketing/TicketingApplicationTests.java
+++ ticketing/src/test/java/org/truve/platform/ticketing/TicketingApplicationTests.java
@@ -2,10 +2,15 @@ package org.truve.platform.ticketing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.truve.platform.ticketing.service.TicketingApplication;
+import org.truve.platform.ticketing.service.booking.external.client.payment.PaymentClient;
 
 @SpringBootTest(classes = TicketingApplication.class)
 class TicketingApplicationTests {
+
+	@MockitoBean
+	PaymentClient paymentClient;
 
 	@Test
 	void contextLoads() {
