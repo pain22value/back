ae0f1ef09c79440bb51f01c1156f54a1620629f5
YeKim1 <12202340@inha.edu>
2026-04-17 11:33:52 +0900

    feat: PaymentApplication @EnableScheduling 추가

--- payment/src/main/java/com/truve/platform/payment/service/PaymentApplication.java
+++ payment/src/main/java/com/truve/platform/payment/service/PaymentApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.truve.platform")
 @ConfigurationPropertiesScan
 @EnableAsync
+@EnableScheduling
 public class PaymentApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
