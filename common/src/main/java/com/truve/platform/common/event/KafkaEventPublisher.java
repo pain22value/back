b6583a6528b151eb66c96472b13f161521c1ebb4
YeKim1 <12202340@inha.edu>
2026-04-17 11:26:15 +0900

    refactor: KafkaEventPublisher에서 JsonConverter를 사용하도록 변경

--- common/src/main/java/com/truve/platform/common/event/KafkaEventPublisher.java
+++ common/src/main/java/com/truve/platform/common/event/KafkaEventPublisher.java
@@ -4,10 +4,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.truve.platform.common.exception.CustomException;
-import com.truve.platform.common.exception.ErrorCode;
+import com.truve.platform.common.support.JsonConverter;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,34 +15,24 @@ import lombok.extern.slf4j.Slf4j;
 public class KafkaEventPublisher implements EventPublisher {
 
 	private final KafkaTemplate<String, String> kafkaTemplate;
-	private final ObjectMapper objectMapper;
+	private final JsonConverter jsonConverter;
 
 	@Override
 	public void publish(String topic, String key, Object event) {
-		try {
-			String payload = objectMapper.writeValueAsString(event);
-			log.info("[Kafka Publish] Start - Topic: {}, Key: {}, Payload: {}", topic, key, payload);
-			
-			kafkaTemplate.send(topic, key, payload);
-		} catch (JsonProcessingException exception) {
-			throw new CustomException(ErrorCode.EVENT_SERIALIZATION_FAILED);
-		} catch (Exception exception) {
-			throw new CustomException(ErrorCode.EVENT_PUBLISH_FAILED);
-		}
+		String payload = jsonConverter.serialize(event);
+		log.info("[Kafka Publish] Start - Topic: {}, Key: {}, Payload: {}", topic, key, payload);
+
+		kafkaTemplate.send(topic, key, payload);
 	}
 
 	@Override
 	public void publish(String topic, String key, String type, Object event) {
-		try {
-			String payload = objectMapper.writeValueAsString(event);
-			log.info("[Kafka Publish With Header] Topic: {}, event-type: {}, Payload: {}", topic, type, payload);
+		String payload = jsonConverter.serialize(event);
+		log.info("[Kafka Publish With Header] Topic: {}, event-type: {}, Payload: {}", topic, type, payload);
 
-			ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, payload);
-			record.headers().add("event-type", type.getBytes());
+		ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, payload);
+		record.headers().add("event-type", type.getBytes());
 
-			kafkaTemplate.send(record);
-		} catch (JsonProcessingException e) {
-			throw new CustomException(ErrorCode.EVENT_SERIALIZATION_FAILED);
-		}
+		kafkaTemplate.send(record);
 	}
 }
